0c5175a2d1fd865857705dd67f4e8ee0ad8ae715
asariia <apsarira@gmail.com>
2023-07-16 15:49:57 +0700

    rubah lagi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -127,7 +127,7 @@ const App = (props: ExtendedAppProps) => {
         <title>{`${themeConfig.templateName} - Penuhi Jiwa dengan Keindahan Seni`}</title>
         <meta
           name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+          content={`${themeConfig.templateName} – Penuhi Jiwa dengan Keindahan Seni`}
         />
         <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
